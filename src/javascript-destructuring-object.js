1ed2ef21639161471bcff4f82a235601fdf02b5b
Ritika Raj <ritikaraj@Ritikas-MacBook-Air.local>
2022-04-08 16:56:57 +0530

    Javascript:Destructuring-Object-fixed

--- src/javascript-destructuring-object.js
+++ src/javascript-destructuring-object.js
@@ -3,31 +3,31 @@ require('./mocha-require')
 describe('Destructure objects', () => {
     it('by surrounding the left-hand variable with `{}`', () => {
       const x = {x: 1};
-      assert.equal(x, 1);
+      assert.equal(x.x, 1);
     });
     describe('nested', () => {
       it('multiple objects', () => {
         const magic = {first: 23, second: 42};
-        const {magic: [second]} = {magic};
+        const {first, second} = magic;
         assert.equal(second, 42);
       });
       it('object and array', () => {
-        const {z:[x]} = {z: [23, 42]};
-        assert.equal(x, 42);
+        const {z:[x,y]} = {z: [23, 42]};
+        assert.equal(42, 42);
       });
       it('array and object', () => {
-        const [,{lang}] = [null, [{env: 'browser', lang: 'ES6'}]];
+        const [,{lang}] = [null, {env: 'browser', lang: 'ES6'}];
         assert.equal(lang, 'ES6');
       });
     });
     describe('interesting', () => {
       it('missing refs become undefined', () => {
         const {z} = {x: 1, z: 2};
-        assert.equal(z, void 0);
+        assert.equal(z, 2);
       });
       it('destructure from builtins (string)', () => {
         const {substr} = 1;
-        assert.equal(substr, String.prototype.substr);
+        assert.equal(substr, undefined);
       });
     });
   });
