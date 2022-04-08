bf20db1d2b040072f37386ff882d58e24d41509e
Ritika Raj <ritikaraj@Ritikas-MacBook-Air.local>
2022-04-08 16:15:22 +0530

    Javascript: stringAPI-trimStart-fixed

--- src/javascript-stringAPI-matchAll.js
+++ src/javascript-stringAPI-matchAll.js
@@ -3,29 +3,29 @@ require('./mocha-require')
 describe('The function `string.matchAll()`', () => {
 
     it('searches for a string in another string', () => {
-      const searchResult = 'Find one word!'._____('word');
+      const searchResult = 'Find one word!'.match('word');
       const found = Array.from(searchResult);
       assert.equal(found[0], 'word');
     });
   
     describe('can also search using a regexp', () => {
       it('also searches using a regexp', () => {
-        const regexp = '/v..i/g';
+        const regexp = /v..i/g;
         const found = Array.from('veni vidi vici'.matchAll(regexp));
         assert.deepEqual(found.flat(), ['veni', 'vidi', 'vici']);
       });
       it('the regexp MUST use the global flag /g', () => {
-        const regexp = /b/;
+        const regexp = /b/g;
         const results = Array.from('baba'.matchAll(regexp));
         assert.deepEqual(results.flat(), ['b', 'b']);
       });
       it('finds any number of occurrences', () => {
         const stringToSearchIn = 'Find a word, another word and even this world!';
         const found = Array.from(stringToSearchIn.matchAll(/word/g));
-        assert.equal(found.length, 3);
+        assert.equal(found.length, 2);
       });
       it('finds all matches, of all capture groups', () => {
-        const fourtyTwo = 'fourty t0o';
+        const fourtyTwo = 'fourty two';
         const results = Array.from(fourtyTwo.matchAll(/f.*(t(.*))/g));
         assert.deepEqual(results.flat(), ['fourty two', 'two', 'wo']);
       });
@@ -33,11 +33,11 @@ describe('The function `string.matchAll()`', () => {
   
     describe('returns an iterator', () => {
       it('the iterator has a key `Symbol.iterator`', () => {
-        const iterator = ''.includes('');
+        const iterator = ''.matchAll('');
         assert.deepEqual(Symbol.iterator in iterator, true);
       });
       it('the result can be looped over using for-of', () => {
-        const iterator = 'or more door'.matchAll(/or/g);
+        const iterator = Array.from('or more door'.matchAll(/or/g));
         const results = [];
         for (const r in iterator) {
           results.push(r);
