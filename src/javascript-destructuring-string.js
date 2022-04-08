966d1f8f78951158023446bf0ff2ad99d4bc2fe4
Ritika Raj <ritikaraj@Ritikas-MacBook-Air.local>
2022-04-08 16:37:34 +0530

    Javascript:Destructuring-String-without fix

---
+++ src/javascript-destructuring-string.js
@@ -0,0 +1,16 @@
+require('./mocha-require') 
+
+describe('Destructuring also works on strings', () => {
+    it('destructure every character, just as if the string was an array', () => {
+      let a, b, c = 'abc';
+      assert.deepEqual([a, b, c], ['a', 'b', 'c']);
+    });
+    it('missing characters are undefined', () => {
+      const [a, c] = 'ab';
+      assert.equal(c, void 0);
+    });
+    it('unicode character work too', () => {
+      const [space, coffee] = 'a ☕';
+      assert.equal(coffee, '\u{2615}');
+    });
+  });
